dd46f04c7458ec2dd2491f3972572127fcd857f3
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-11 14:48:30 +0200

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>
+    <a href="#home"> </a>
+    <a href="#about"> </a>
+    </nav>;
 }
 
 export default NavBar;
